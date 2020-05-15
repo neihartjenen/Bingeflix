c990a0f9afa05eae5f6a955da5bfde99443f26b5
lindsey tummond <lindseytummond@gmail.com>
2020-05-14 23:06:22 -0700

    connecting to mongodb

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,9 @@ module.exports = {
     google:{
         clientID:'484705538914-3k9j275kq5dqd6jfiq6f5m8lm9vufqps.apps.googleusercontent.com',
         clientSecret: '85dn3X-9cdoreD_bZiOZiZvX',
+    },
+
+    mongodb:{
+        dbURI: 'mongodb+srv://lindseytummond:x5y6eLST$@cluster0-emttv.mongodb.net/test?retryWrites=true&w=majority',
     }
 }
--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 // =============================================================
 var express = require("express");
 var authRoutes = require('./routes/auth-routes');
-var passportSetup = require('./config/passport-setup')
+var passportSetup = require('./config/passport-setup');
+const Sequelize = require('sequelize');
+var keys = require('./config/keys')
 
 // Sets up the Express App
 // =============================================================
@@ -11,6 +13,11 @@ var PORT = process.env.PORT || 3000;
 
 //set up view engine
 app.set('view engine', 'ejs');
+
+//connect to mongodb
+Sequelize.connect(keys.mongodb.dbURI, () => {
+    console.log('connected to mongodb')
+})
 
 //set up routes
 app.use('/auth', authRoutes);
