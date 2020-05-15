b7de0b95c3c542dbe48d75513387288c55b358a5
lindsey tummond <lindseytummond@gmail.com>
2020-05-14 21:27:58 -0700

    securing keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//added rto .gitignore
+
+module.exports = {
+    google:{
+        clientID:'484705538914-3k9j275kq5dqd6jfiq6f5m8lm9vufqps.apps.googleusercontent.com',
+        clientSecret: '85dn3X-9cdoreD_bZiOZiZvX',
+    }
+}
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,10 +2,13 @@
 // =============================================================
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var keys = reqiure(./keys);
 
 passport.use(
     new GoogleStrategy({
         // options for google strategy
+        clientID:keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
     }, () => {
         // passport callback function
     })
