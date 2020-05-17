432b4dbcd34816bf5954f4770f497a4a956daef7
Dylan Ambrose <djambro44@gmail.com>
2020-05-17 13:46:06 -0700

    basic API set up

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,22 @@
 //needs jquery CDN
 
 $( document ).ready(function() {
+
+    var createContent = function(data) {
+        //created new p tag
+        var p = $("p");
+
+        var title = $("<p>").text(data.Title);
+        var actors = $("<p>").text(data.Actors);
+        var year = $("<p>").text(data.Year);
+        var plot = $("<p>").text(data.Plot);
+        // var poster = $("<img>").src(data.poster);
+
+        p.append(title, actors, year, plot);
+
+        $("#searchResults").append(p)
+        
+    }
     
     
     var searchOMDB = function(movie) {
@@ -9,9 +25,15 @@ $( document ).ready(function() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            createRow(response);
+            console.log(response);
+            createContent(response);
         });
     };
     
+    $("#search").on("click", function(){
+        var movie = document.getElementById("userInput").value;
+        searchOMDB(movie)
+        console.log(movie);
+    });
     
 });
