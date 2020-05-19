00090a1ddeafc5cd117d1a140beca789d2d683d4
lindsey tummond <lindseytummond@gmail.com>
2020-05-18 23:32:58 -0700

    creating users.js file to insert user info into db using sequelize

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS bingeflix_db;
+CREATE DATABASE bingeflix_db;
+
+USE bingeflix_db;
--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var authRoutes = require('./routes/auth-routes');
-var passportSetup = require('./config/passport-setup');
-var mongoose = require('mongoose');
-var keys = require('./config/keys')
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-//set up view engine
-app.set('view engine', 'ejs');
-
-//connect to mongodb
-mongoose.connect(keys.mongodb.dbURI, () => {
-    console.log('connected to mongodb')
-})
-
-//set up routes
-app.use('/auth', authRoutes);
+app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
 //create home route
 app.get('/', (req, res) => {
-    res.render('home');
+    res.send('Welcome to Passport with Sequelize and without HandleBars');
 });
 
 
@@ -35,3 +26,4 @@ var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
