97c79e0d910c1b063a28f6b4e40a55a6e68e6c4d
lindsey tummond <lindseytummond@gmail.com>
2020-05-19 00:27:53 -0700

    removing dependencies no longer needed

---
+++ routes/html-routes.js
@@ -0,0 +1,31 @@
+// require path for relative routes to HTML files
+var path = require("path");
+
+// requirie middleware for checking if user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+
+    // if the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // if the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // isAuthenticated middleware added to route
+  // if a user who is not logged in tries to access route; will be 
+  // redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+};
