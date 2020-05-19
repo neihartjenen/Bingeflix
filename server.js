f7d5d5735a9218135a05232b01bbb97c6a5489b6
lindsey tummond <lindseytummond@gmail.com>
2020-05-18 23:39:45 -0700

    require server to read models folder

--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+var db = require("./models");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 
 app.use(bodyParser.urlencoded({ extended: false })); //For body parser
 app.use(bodyParser.json());
@@ -22,8 +24,8 @@ app.get('/', (req, res) => {
 
 // Starts the server to begin listening
 // =============================================================
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 
