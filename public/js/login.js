91b11f275f7961d8cad936bd0118e44d3acd12a8
lindsey tummond <lindseytummond@gmail.com>
2020-05-20 13:06:34 -0700

    post route

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  // Getting references to our form and inputs
+  // obtainging references to our form and inputs
   var loginForm = $("form.login");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
-  // When the form is submitted, we validate there's an email and password entered
+  // when form is submitted, we validate there's an email and password entered
   loginForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
@@ -16,13 +16,13 @@ $(document).ready(function() {
       return;
     }
 
-    // If we have an email and password we run the loginUser function and clear the form
+    // if we have an email and password we run the loginUser function and clear the form
     loginUser(userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
   });
 
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+  // loginUser does a post to our "api/login" route and if successful, redirects to the members page
   function loginUser(email, password) {
     $.post("/api/login", {
       email: email,
