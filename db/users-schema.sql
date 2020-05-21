c3bea2f3e18773784b24930388045b4920742101
lindsey tummond <lindseytummond@gmail.com>
2020-05-20 23:16:01 -0700

    fixing undefined function bug

--- db/users-schema.sql
+++ db/users-schema.sql
@@ -3,9 +3,11 @@ CREATE DATABASE bingeflix_db;
 
 USE bingeflix_db;
 
-CREATE TABLE user (
+CREATE TABLE Users (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(50) NOT NULL,
+  createdAt datetime,
+  updatedAt datetime,
   PRIMARY KEY (id)
 );
