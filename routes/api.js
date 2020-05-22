ad0629b70d568a79e606bee8105b1818583fd7b2
Dylan Ambrose <djambro44@gmail.com>
2020-05-22 16:46:36 -0700

    got the featured shows hooked up to API to show img

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 //needs jquery CDN
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    var createContent = function(data) {
+    var createContent = function (data) {
         //created new p tag
         var p = $("<p>");
 
@@ -23,29 +23,72 @@ $( document ).ready(function() {
 
         $("#searchResults").prepend(resultsDiv);
 
-       
+
     }
-    
-    
-    var searchOMDB = function(movie) {
+
+
+    var searchOMDB = function (movie) {
         var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function (response) {
             console.log(response);
             createContent(response);
         });
     };
-    
-    $("#search").on("click", function(){
+
+
+    function clear() {
+        $("#searchResults").empty();
+    }
+
+
+    $("#search").on("click", function (event) {
+        //this loads our search results page on button click
+        // window.location = "searchPage.html";
+        //supposed to allow enter press to search but not working
+        event.preventDefault();
+        //clears data from last search
+        clear();
+
+        //saves user input as the movie/show being searched for
         var movie = document.getElementById("userInput").value;
+        //runs search movie
         searchOMDB(movie)
         console.log(movie);
     });
 
-    // document.getElementById("#search").onclick = function () {
-    //     location.href = "searchPage.html";
-    // } 
     
+    
+    var featuredData = function(img, movie) {
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+            featuredContent(img, response);
+        });
+    };
+    
+    
+    function featuredContent(img, data){
+
+        // var featuredDiv = $("#featured")
+        
+        // var poster = $("<img>");
+        // poster.attr("src", data.Poster);
+        // featuredDiv.prepend(poster);
+
+       img.attr("src", data.Poster);
+        // $("#featured2").attr("src", data.Poster);
+        // $("#featured3").attr("src", data.Poster);
+    }
+
+//    var featuredShows = ["the office", "friends", "outer banks"];
+
+   featuredData($("#featured1"),  "the office");
+   featuredData($("#featured2"), "friends");
+   featuredData($("#featured3"), "outer banks");
+   
 });
